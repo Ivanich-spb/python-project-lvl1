4ad6ea550ba27a9a03ea1505ff3a5fc61581bd9d
Ivanich <zvoox.ru@gmail.com>
2020-04-17 04:36:12 +0300

    Add Makefile and cli.py

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3
 
+from cli import welcome_user
+
+def welcome():
+    print('Welcome to the Brain Games!')
+
 
 def main():
-    print('Welcome to the Brain Games!')
+    welcome()
+    welcome_user()
 
 
 if __name__ == '__main__':
